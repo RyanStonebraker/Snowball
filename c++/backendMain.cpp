6fe27eda9e6eed128ed9ed3bbcf860e22a56d730
tvancise <tvancise@alaska.edu>
2018-01-26 22:39:39 -0900

    move generation involving multiple hops works for normal red pieces that are not on the side - TO DO: red king move generation & movement when piece is on the side of the board

--- c++/backendMain.cpp
+++ c++/backendMain.cpp
@@ -1,9 +1,26 @@
-/*
+﻿/*
 main.cpp
 Snowball AI
 CS 405 - Dr. Genetti
 All main functions that pertain to generating valid moves will be
 called from here to keep things organized.
+*/
+
+
+/*
+			THE BOARD
+
+	_________________________
+	|0 |  | 1|  | 2|  | 3|  |
+	|  |4 |  |5 |  |6 |  |7 |
+	|8 |  |9 |  |10|  |11|  |
+	|  |12|  |13|  |14|  |15|
+	|16|  |17|  |18|  |19|  |
+	|  |20|  |21|  |22|  |23|
+	|24|  |25|  |26|  |27|  |
+	|  |28|  |29|  |30|  |31|
+	‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+
 */
 
 #include "backend.h"
@@ -37,14 +54,75 @@ called from here to keep things organized.
 bool canMove(int position, int nextPosition, int piece, std::string boardString)
 {
 	bool isSide = position == 0 || position == 8 || position == 16 || position == 24 ||
-				  position == 7 || position == 15 || position == 23 || position == 31;
+		position == 7 || position == 15 || position == 23 || position == 31;
 
-	if (isSide)
+	int nextLocationState = boardString[nextPosition] - '0';
+
+	if (isSide || nextPosition > 31 || nextPosition < 0)
 		return false;
 
-	if (piece == RED) //TO DO: side checks in here
-		return (boardString[nextPosition]-'0') == 0;
+	if (piece == BLACK) //TO DO: side checks in here
+	{
+		return nextLocationState == 0;
+	}
 		
+}
+
+std::string checkKill(int position, const board & currentBoard, std::string & visited)
+{
+	std::string nextBoard = currentBoard.getBoardStateString();
+
+	bool leftKillCondition = (canMove(position, position - 9, currentBoard[position], nextBoard)) &&
+							 (visited[position - 9] != 'x') &&
+							 (currentBoard[position - 4] == RED ||
+							 currentBoard[position - 4] == RED_KING);
+
+	bool rightKillCondition = (canMove(position, position - 7, currentBoard[position], nextBoard)) &&
+							  (visited[position - 7] != 'x') &&
+							  (currentBoard[position - 3] == RED ||
+							  currentBoard[position - 3] == RED_KING);
+
+	if (!(leftKillCondition && rightKillCondition))
+		visited[position] = 'x';
+	
+
+	if (leftKillCondition) //left kill condition
+	{
+		std::swap(nextBoard[position], nextBoard[position - 9]);
+		nextBoard[position - 4] = '0';
+
+		/*if(!rightKillCondition)
+			visited[position - 9] = 'x';*/
+
+		return checkKill(position - 9, { nextBoard }, visited);
+	}
+
+	if (rightKillCondition) //right kill condition
+	{
+		std::swap(nextBoard[position], nextBoard[position - 7]);
+		nextBoard[position - 3] = '0';
+
+		/*if(!leftKillCondition)
+			visited[position - 7] = 'x';*/
+		
+		return checkKill(position - 7, { nextBoard }, visited);
+	}
+
+	return nextBoard;
+
+}
+
+void workhorse(int position, const board & currentBoard, std::vector<board> & validMoves, std::string visited)
+{
+	while (true)
+	{
+		std::string nextBoard = checkKill(position, currentBoard, visited);
+
+		if (nextBoard == currentBoard.getBoardStateString())
+			break;
+
+		validMoves.push_back({ nextBoard });
+	}
 }
 
 std::vector<board> generateRandomMoves(board currentBoard)
@@ -54,25 +132,31 @@ std::vector<board> generateRandomMoves(board currentBoard)
 
 	std::vector<board> validMoves;
 
-	for (int i = 0; i < currentBoard.getBoardStateString().length(); i++)
+	for (int position = 0; position < currentBoard.getBoardStateString().length(); position++)
 	{
 		std::string nextBoard = currentBoard.getBoardStateString();
 
-		if ((nextBoard[i]-'0') == RED)
+		if (currentBoard[position] == BLACK)
 		{
-			if (canMove(i, i + 4, currentBoard[i], nextBoard)) //can we move left?
+			if (canMove(position, position - 4, currentBoard[position], nextBoard)) //can we move left?
 			{
-				std::swap(nextBoard[i], nextBoard[i + 4]);
-				board temp(nextBoard);
-				validMoves.push_back(temp);
+				std::swap(nextBoard[position], nextBoard[position - 4]);
+				validMoves.push_back({ nextBoard });
+			}
+			else
+			{
+				workhorse(position, currentBoard, validMoves, currentBoard.getBoardStateString());
 			}
 
-			if (canMove(i, i + 5, currentBoard[i], nextBoard)) //can we move left?
+			if (canMove(position, position - 3, currentBoard[position], nextBoard)) //can we move right?
 			{
 				nextBoard = currentBoard.getBoardStateString();
-				std::swap(nextBoard[i], nextBoard[i + 5]);
-				board temp(nextBoard);
-				validMoves.push_back(temp);
+				std::swap(nextBoard[position], nextBoard[position - 3]);
+				validMoves.push_back({ nextBoard });
+			}
+			else
+			{
+				workhorse(position, currentBoard, validMoves, currentBoard.getBoardStateString());
 			}
 		}
 
@@ -86,6 +170,7 @@ int main()
 	board b(readBoardState());
 	std::vector<board> validMoves = generateRandomMoves(b);
 	
+	std::cout << "Black Center Move Generations: \n" << std::endl;
 	for (const auto & n : validMoves)
 	{
 		std::cout << n << std::endl;
