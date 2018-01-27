424b63bf206950ba0767b6eb391b250372f1b4eb
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-01-27 02:36:17 -0900

    Synchronous play states, board to string, refactored to be an Electron app

---
+++ index.js
@@ -0,0 +1,8 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({ resizable: false, width: 850, height: 625});
+  mainWindow.loadURL(path.join('file://', __dirname, "index.html"));
+});
