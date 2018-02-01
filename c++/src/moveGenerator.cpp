48020144248b77a032f8173d8bf13c75da579570
tvancise <tvancise@alaska.edu>
2018-02-01 12:36:49 -0900

    fixing piece change (had == instead of =)

--- c++/src/moveGenerator.cpp
+++ c++/src/moveGenerator.cpp
@@ -120,12 +120,12 @@ std::string workhorse(int position, const Board & currentBoard, std::vector<Boar
 
 		if (nextBoard[0] == BLACK)
 			nextBoard[0] == BLACK_KING;
-		else if (nextBoard[1] == BLACK)
+		else if (nextBoard[1] = BLACK)
 			nextBoard[1] == BLACK_KING;
-		else if (nextBoard[2] == BLACK)
+		else if (nextBoard[2] = BLACK)
 			nextBoard[2] == BLACK_KING;
-		else if (nextBoard[3] == BLACK)
-			nextBoard[3] == BLACK_KING;
+		else if (nextBoard[3] = BLACK)
+			nextBoard[3] = BLACK_KING;
 
 		validMoves.push_back({ nextBoard });
 	}
