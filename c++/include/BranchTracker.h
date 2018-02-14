409f61ae5d744792060bec076d5ae5fe17b0638f
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-02-14 02:11:40 -0900

    Fully written BranchTracker that in theory should return the best board in a given depth, but relies on features that dont yet exist in the Neuron class and therefore HAS NOT BEEN TESTED.

--- c++/include/BranchTracker.h
+++ c++/include/BranchTracker.h
@@ -15,6 +15,13 @@
 #include <list>
 #include <tuple>
 
+struct NodeFactors {
+  std::tuple<int,int> totalKings;
+  std::tuple<int,int> totalMovesAvailable;
+  std::tuple<int,int> totalQuality;
+  unsigned int childrenAmount;
+};
+
 // A class that recursive/iteratively goes through every possible future move
 // to a depth specified in the WeightedNode and weights various components to
 // come up with a cumulative score of branch. The lowest percentage of same level
@@ -37,19 +44,25 @@ public:
   // double getKingingChange();
   // double getQuality();
 
-  Board getBestMove();
+  // gets the best move by calling a recursive traversal function.
+  // takes an enumerated Color type (RED_PIECE/BLACK_PIECE)
+  Board getBestMove(Color);
 
   Neuron fastForwardHead();
 
   // appends current board to private variables
   // double getCurrentBoardWeight();
 
-  // recursive/iterative function to go through all children down to a depth of
-  // _branchWeightings.depth
-  double cumulativeBranchWeight();
-
 // private helper functions
 private:
+  // recursive/iterative function to go through all children down to a depth of
+  // _branchWeightings.depth
+  NodeFactors _cumulativeBranchWeight(Neuron, unsigned int);
+
+  float _sigmoidNormalizer(double);
+
+  // Gets the weight of the branch for given color before sigmoid function
+  double raw_weighting(NodeFactors, Color);
 
 // private members
 private:
@@ -60,9 +73,16 @@ private:
   std::tuple<int,int> _totalMovesAvailable;
   std::tuple<int,int> _totalQuality;
 
-  std::tuple<int,int> _maxKings;
-  std::tuple<int,int> _maxMoves;
-  std::tuple<int,int> _maxQuality;
+  // TODO: Do something with max counts and risk factor later...
+  // std::tuple<int,int> _maxKings;
+  // std::tuple<int,int> _maxMoves;
+  // std::tuple<int,int> _maxQuality;
+
+  unsigned int _childrenAmount;
+
+  float _weight;
+
+
 
   // temp neuron to explore a path before appending to the global _head Neuron
   Neuron _localBranch;
--- c++/src/BranchTracker.cpp
+++ c++/src/BranchTracker.cpp
@@ -7,16 +7,98 @@
 #include "BranchTracker.h"
 #include "neuron.h"
 #include "board.h"
+#include <math.h>
 
 // 2 param ctor for BranchTracker, NOTE WeightedNode is passed by reference to
 // allow for updating given a branches returned values
 BranchTracker::BranchTracker(const Board &startBoard, WeightedNode &branchWeight)
-    : _branchWeightings(branchWeight), _startBoard(startBoard) {
+    : _branchWeightings(branchWeight), _startBoard(startBoard), _childrenAmount(0) {
   // Update Neuron starting board
   _localBranch = Neuron(this->_startBoard);
 }
 
-Board BranchTracker::getBestMove() {
-  
-  return this->_startBoard;
+Board BranchTracker::getBestMove(BranchTracker::Color pieceColor) {
+  this->_localBranch.spawnChildren(this->_branchWeightings.depth);
+
+  // update local info with best cumulative info
+  for (unsigned int child = 0; child < this->_localBranch.childrenSize(); ++child) {
+    NodeFactors firstChildFactor = _cumulativeBranchWeight(this->_localBranch[child], this->_branchWeightings.depth);
+    float childWeight = _sigmoidNormalizer(raw_weighting(firstChildFactor, pieceColor));
+    if (childWeight > this->_weight) {
+      this->_bestMoveNode = this->_localBranch[child];
+
+      this->_totalKings = firstChildFactor.totalKings;
+      this->_totalMovesAvailable = firstChildFactor.totalMovesAvailable;
+      this->_totalQuality = firstChildFactor.totalQuality;
+
+      this->_weight = childWeight;
+    }
+  }
+
+  return this->_bestMoveNode.getBoard();
+}
+
+float BranchTracker::_sigmoidNormalizer (double raw_weight) {
+  return tanh(raw_weight);
+}
+
+double BranchTracker::raw_weighting(NodeFactors factors, BranchTracker::Color pieceColor) {
+  double raw_total = 0;
+
+  Color enemyColor = (pieceColor == RED_PIECE) ? BLACK_PIECE : RED_PIECE;
+
+  double kingFactor = std::get<pieceColor>(this->_totalKings) / this->_childrenAmount * _branchWeightings.kingingWeight;
+  double freedomFactor = std::get<pieceColor>(this->_totalMovesAvailable) / this->_childrenAmount * _branchWeightings.availableMovesWeight;
+  double qualityFactor = std::get<pieceColor>(this->_totalQuality) / this->_childrenAmount * _branchWeightings.qualityWeight;
+
+  double enemyKingFactor = std::get<enemyColor>(this->_totalKings) / this->_childrenAmount * _branchWeightings.kingingWeight * _branchWeightings.enemyFactor;
+  double enemyFreedomFactor = std::get<enemyColor>(this->_totalMovesAvailable) / this->_childrenAmount * _branchWeightings.availableMovesWeight * _branchWeightings.enemyFactor;
+  double enemyQualityFactor = std::get<enemyColor>(this->_totalQuality) / this->_childrenAmount * _branchWeightings.qualityWeight * _branchWeightings.enemyFactor;
+
+  raw_total = kingFactor + freedomFactor + qualityFactor - enemyKingFactor - enemyFreedomFactor - enemyQualityFactor;
+  return raw_total;
+}
+
+NodeFactors BranchTracker::_cumulativeBranchWeight(Neuron temp_head, unsigned int depthLimit) {
+  std::tuple<int,int> tempTotalKings;
+  std::tuple<int,int> tempTotalMovesAvailable;
+  std::tuple<int,int> tempTotalQuality;
+
+  NodeFactors tempFactors;
+
+  unsigned int tempNumChildren = 0;
+
+    // Loop through every child of temp_head
+  for (unsigned int child = 0; child < temp_head.childrenSize(); ++child) {
+    std::get<RED_PIECE>(tempTotalKings) += temp_head[child].getRedKings();
+    std::get<BLACK_PIECE>(tempTotalKings) += temp_head[child].getBlackKings();
+
+    std::get<RED_PIECE>(tempTotalMovesAvailable) += temp_head[child].getRedMovesAvailable();
+    std::get<BLACK_PIECE>(tempTotalMovesAvailable) += temp_head[child].getBlackMovesAvailable();
+
+    std::get<RED_PIECE>(tempTotalQuality) += temp_head[child].getRedQuality();
+    std::get<BLACK_PIECE>(tempTotalQuality) += temp_head[child].getBlackQuality();
+
+    tempFactors.totalKings = tempTotalKings;
+    tempFactors.totalMovesAvailable = tempTotalMovesAvailable;
+    tempFactors.totalQuality = tempTotalQuality;
+
+    // if child has children and haven't reached our depth limit, then recursive case
+    if (temp_head[child].childrenSize() > 0 && depthLimit > 0) {
+      NodeFactors childFactors = _cumulativeBranchWeight(temp_head[child], depthLimit - 1);
+
+      tempFactors.totalKings += childFactors.totalKings;
+      tempFactors.totalMovesAvailable += childFactors.totalMovesAvailable;
+      tempFactors.totalQuality += childFactors.totalQuality;
+      tempNumChildren += tempFactors.childrenAmount;
+    }
+
+    ++tempNumChildren;
+  }
+  return tempFactors;
+}
+
+Neuron BranchTracker::fastForwardHead() {
+  // TODO: Do error checking?
+  return this->_bestMoveNode;
 }
