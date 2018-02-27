e9916759943c01de188033acb76c64665b214af3
thatcherlane <thatcherlane@gmail.com>
2018-02-27 14:27:57 -0900

    changing paramaters and adding assignments to evolveChile()

--- c++/include/neuron.h
+++ c++/include/neuron.h
@@ -6,40 +6,40 @@
 
 class Neuron {
 
-	//Equality overloads
-	friend bool operator==(Neuron & lhs, Neuron & rhs);
-	friend bool operator!=(Neuron & lhs, Neuron & rhs);
+    //Equality overloads
+    friend bool operator==(Neuron & lhs, Neuron & rhs);
+    friend bool operator!=(Neuron & lhs, Neuron & rhs);
 
 public:
-	Neuron() = default;
-	Neuron(Board &board);
-	Neuron(float, float, std::vector<std::shared_ptr<Neuron>> &, float, Board);
-	Neuron(Neuron & other);
-	Neuron & operator=(Neuron & other);
-	void spawnChildrenBlackFirst(int depth);
-	void spawnChildrenRedFirst(int depth);
-	void updateAverageWeights();
-	size_t size() const;
+    Neuron() = default;
+    Neuron(Board &board);
+    Neuron(float, float, std::vector<std::shared_ptr<Neuron>> &, float, Board);
+    Neuron(Neuron & other);
+    Neuron & operator=(Neuron & other);
+    void spawnChildrenBlackFirst(int depth);
+    void spawnChildrenRedFirst(int depth);
+    void updateAverageWeights();
+    size_t size() const;
 
-	int getQuality() const;
-	int getRedKingCount() const;
-	int getBlackKingCount() const;
-	int getBlackPieceCount() const;
-	int getRedPieceCount() const;
+    int getQuality() const;
+    int getRedKingCount() const;
+    int getBlackKingCount() const;
+    int getBlackPieceCount() const;
+    int getRedPieceCount() const;
 
-	// Accessors for Neuron
-	float getWeight() const;
-	float getAverageWeight() const;
-	std::vector<std::shared_ptr<Neuron>> getChildren();
-	float getRiskFactor() const;
-	Board getBoard();
+    // Accessors for Neuron
+    float getWeight() const;
+    float getAverageWeight() const;
+    std::vector<std::shared_ptr<Neuron>> getChildren();
+    float getRiskFactor() const;
+    Board getBoard();
 
     void setWeight(float weight);
     void setAverageWeight(float averageWeight);
     void setRiskFactor(float riskFactor);
 
-	//[] overloads
-	std::shared_ptr<Neuron> & operator[](int index);
+    //[] overloads
+    std::shared_ptr<Neuron> & operator[](int index);
     friend std::ostream & operator<<(std::ostream&, const Neuron&);
     friend std::istream& operator>>(std::istream &, Neuron&);
 
@@ -48,19 +48,22 @@ public:
     float kingWeightPrime(float oldKingWeight);
     float sigmaWeightPrime(float oldSigmaWeight, int numberOfWeights);
     float nodeWeightPrime(float oldNodeWeight, float sigmaWeight);
-    Neuron& evolveChild(Neuron&);
+    Neuron& evolveChild();
+
+
 
 private:
-	std::vector<Board> spawnBlack (Board & initBoard);
-	std::vector<Board> spawnRed (Board & rBoard);
-	std::vector<Board> spawnRedOnce (Board & rBoard);
-	std::shared_ptr<Neuron> recurseSpawning(int depth, std::shared_ptr<Neuron> parentNeuron);
+    std::vector<Board> spawnBlack (Board & initBoard);
+    std::vector<Board> spawnRed (Board & rBoard);
+    std::vector<Board> spawnRedOnce (Board & rBoard);
+    std::shared_ptr<Neuron> recurseSpawning(int depth, std::shared_ptr<Neuron> parentNeuron);
 
-	float _weight;
-	float _averageWeight;
-	std::vector<std::shared_ptr<Neuron>> _children;
-	float _riskFactor;
-	std::shared_ptr<Board> _board;
+    float _weight;
+    float _averageWeight;
+    std::vector<std::shared_ptr<Neuron>> _children;
+    float _riskFactor;
+    std::shared_ptr<Board> _board;
 };
 
 #endif
+
--- c++/src/neuron.cpp
+++ c++/src/neuron.cpp
@@ -16,189 +16,189 @@ using std::string;
 
 vector<Board> _flipColor(vector<Board> validMoves)
 {
-	//std::move
-	std::transform(validMoves.begin(), validMoves.end(), validMoves.begin(), [](Board & b) {
+    //std::move
+    std::transform(validMoves.begin(), validMoves.end(), validMoves.begin(), [](Board & b) {
 
-		string flippedBoard = "";
-		string boardString = b.getBoardStateString();
-		int quality = b.getQuality();
+        string flippedBoard = "";
+        string boardString = b.getBoardStateString();
+        int quality = b.getQuality();
 
-		std::reverse(boardString.begin(), boardString.end());
+        std::reverse(boardString.begin(), boardString.end());
 
-		b = { boardString, quality };
+        b = { boardString, quality };
 
-		for (auto & n : b.getBoardStateString())
-		{
-			int piece = n - '0';
+        for (auto & n : b.getBoardStateString())
+        {
+            int piece = n - '0';
 
-			if (piece == BLACK)
-				flippedBoard += '1';
-			else if (piece == RED)
-				flippedBoard += '2';
-			else if (piece == BLACK_KING)
-				flippedBoard += '3';
-			else if (piece == RED_KING)
-				flippedBoard += '4';
-			else
-				flippedBoard += '0';
-		}
+            if (piece == BLACK)
+                flippedBoard += '1';
+            else if (piece == RED)
+                flippedBoard += '2';
+            else if (piece == BLACK_KING)
+                flippedBoard += '3';
+            else if (piece == RED_KING)
+                flippedBoard += '4';
+            else
+                flippedBoard += '0';
+        }
 
-		Board temp(flippedBoard, quality);
-		return temp;
+        Board temp(flippedBoard, quality);
+        return temp;
 
-	});
+    });
 
-	return validMoves;
+    return validMoves;
 }
 
 Neuron::Neuron(Board &board) {
-	_board = make_shared<Board>(board);
+    _board = make_shared<Board>(board);
 }
 
 Neuron::Neuron(float weight, float averageWeight, std::vector<std::shared_ptr<Neuron>> & children, float riskFactor, Board board)
-	: _weight(weight), _averageWeight(averageWeight), _children(children), _riskFactor(riskFactor)
+: _weight(weight), _averageWeight(averageWeight), _children(children), _riskFactor(riskFactor)
 {
-	_board = make_shared<Board>(board);
+    _board = make_shared<Board>(board);
 }
 
 Neuron::Neuron(Neuron & other)
 {
-	_weight = other.getWeight();
-	_averageWeight = other.getAverageWeight();
-	_children = other.getChildren();
-	_riskFactor = other.getRiskFactor();
-	_board = other._board;
+    _weight = other.getWeight();
+    _averageWeight = other.getAverageWeight();
+    _children = other.getChildren();
+    _riskFactor = other.getRiskFactor();
+    _board = other._board;
 }
 
 Neuron & Neuron::operator=(Neuron & other)
 {
-	_weight = other.getWeight();
-	_averageWeight = other.getAverageWeight();
-	_children = other.getChildren();
-	_riskFactor = other.getRiskFactor();
-	_board = other._board;
+    _weight = other.getWeight();
+    _averageWeight = other.getAverageWeight();
+    _children = other.getChildren();
+    _riskFactor = other.getRiskFactor();
+    _board = other._board;
 
-	return *this;
+    return *this;
 }
 
 unsigned int evals = 0;
 
 void Neuron::spawnChildrenBlackFirst(int depth) {
-	auto firstMoveSet = spawnBlack(*_board);
+    auto firstMoveSet = spawnBlack(*_board);
 
-	#pragma omp parallel for
-	for (unsigned possMove = 0; possMove < firstMoveSet.size(); ++possMove) {
-		this->_children.push_back(make_shared<Neuron>(firstMoveSet[possMove]));
+#pragma omp parallel for
+    for (unsigned possMove = 0; possMove < firstMoveSet.size(); ++possMove) {
+        this->_children.push_back(make_shared<Neuron>(firstMoveSet[possMove]));
 
-		this->_children[possMove] = recurseSpawning(depth, this->_children[possMove]);
-	}
+        this->_children[possMove] = recurseSpawning(depth, this->_children[possMove]);
+    }
 
 }
 
 void Neuron::spawnChildrenRedFirst(int depth) {
-	auto firstMoveSet = spawnRedOnce(*_board);
+    auto firstMoveSet = spawnRedOnce(*_board);
 
-	#pragma omp parallel for
-	for (unsigned possMove = 0; possMove < firstMoveSet.size(); ++possMove) {
-		this->_children.push_back(make_shared<Neuron>(firstMoveSet[possMove]));
+#pragma omp parallel for
+    for (unsigned possMove = 0; possMove < firstMoveSet.size(); ++possMove) {
+        this->_children.push_back(make_shared<Neuron>(firstMoveSet[possMove]));
 
-		this->_children[possMove] = recurseSpawning(depth, this->_children[possMove]);
-	}
+        this->_children[possMove] = recurseSpawning(depth, this->_children[possMove]);
+    }
 
 }
 
 std::shared_ptr<Neuron> Neuron::recurseSpawning(int depth, std::shared_ptr<Neuron> parentNeuron) {
-	auto rMoves = spawnRed(*parentNeuron->_board);
-	--depth;
+    auto rMoves = spawnRed(*parentNeuron->_board);
+    --depth;
 
-	#pragma omp parallel for
-	for (auto & rMove : rMoves) {
-		parentNeuron->_children.push_back(make_shared<Neuron>(rMove));
-		if (depth > 0)
-			parentNeuron->_children[parentNeuron->_children.size() - 1] = recurseSpawning(depth - 1, parentNeuron->_children[parentNeuron->_children.size() - 1]);
-	}
+#pragma omp parallel for
+    for (auto & rMove : rMoves) {
+        parentNeuron->_children.push_back(make_shared<Neuron>(rMove));
+        if (depth > 0)
+            parentNeuron->_children[parentNeuron->_children.size() - 1] = recurseSpawning(depth - 1, parentNeuron->_children[parentNeuron->_children.size() - 1]);
+    }
 
-	return parentNeuron;
+    return parentNeuron;
 }
 
 vector<Board> Neuron::spawnBlack (Board & initBoard) {
-		MoveGenerator moveGen;
-		return moveGen.generateRandomMoves(initBoard);
+    MoveGenerator moveGen;
+    return moveGen.generateRandomMoves(initBoard);
 }
 
 vector<Board> Neuron::spawnRedOnce (Board & initBoard) {
-		MoveGenerator moveGen;
-		return _flipColor(moveGen.generateRandomMoves(initBoard));
+    MoveGenerator moveGen;
+    return _flipColor(moveGen.generateRandomMoves(initBoard));
 }
 
 vector<Board> Neuron::spawnRed (Board & rBoard) {
-	MoveGenerator moveGen;
-	auto rMoves = _flipColor(spawnBlack(rBoard));
+    MoveGenerator moveGen;
+    auto rMoves = _flipColor(spawnBlack(rBoard));
 
-	vector <Board> bMoves;
-	#pragma omp parallel for
-	for (auto & firstMove : rMoves) {
-		auto childBoard = moveGen.generateRandomMoves(firstMove);
-		for (auto & child : childBoard) {
-			bMoves.push_back(child);
-		}
-	}
+    vector <Board> bMoves;
+#pragma omp parallel for
+    for (auto & firstMove : rMoves) {
+        auto childBoard = moveGen.generateRandomMoves(firstMove);
+        for (auto & child : childBoard) {
+            bMoves.push_back(child);
+        }
+    }
 
-	return _flipColor(bMoves);
+    return _flipColor(bMoves);
 }
 
 //Accessors for indirect board interfacing
 
 int Neuron::getQuality() const
 {
-	return _board->getQuality();
+    return _board->getQuality();
 }
 
 int Neuron::getRedKingCount() const
 {
-	return _board->getRedKingCount();
+    return _board->getRedKingCount();
 }
 
 int Neuron::getBlackKingCount() const
 {
-	return _board->getBlackKingCount();
+    return _board->getBlackKingCount();
 }
 
 int Neuron::getBlackPieceCount() const
 {
-	return _board->getBlackPieceCount();
+    return _board->getBlackPieceCount();
 }
 
 int Neuron::getRedPieceCount() const
 {
-	return _board->getRedPieceCount();
+    return _board->getRedPieceCount();
 }
 
 //Accessors for Neuron
 
 float Neuron::getWeight() const
 {
-	return _weight;
+    return _weight;
 }
 
 float Neuron::getAverageWeight() const
 {
-	return _averageWeight;
+    return _averageWeight;
 }
 
 std::vector<std::shared_ptr<Neuron>> Neuron::getChildren()
 {
-	return _children;
+    return _children;
 }
 
 float Neuron::getRiskFactor() const
 {
-	return _riskFactor;
+    return _riskFactor;
 }
 
 Board Neuron::getBoard()
 {
-	return *_board;
+    return *_board;
 }
 
 void Neuron::setWeight(float weight){
@@ -215,12 +215,12 @@ void Neuron::setRiskFactor(float riskFactor){
 
 size_t Neuron::size() const
 {
-	return _children.size();
+    return _children.size();
 }
 
 std::shared_ptr<Neuron> & Neuron::operator[](int index)
 {
-	return _children[index];
+    return _children[index];
 }
 
 std::ostream &operator<<(std::ostream& strm, const Neuron& obj){
@@ -271,25 +271,29 @@ float Neuron::nodeWeightPrime(float oldNodeWeight, float sigmaWeight){
 //parameters are not correct for nodeWeightPrime() need to add sigma values
 //using risk factor in place of sigma weights for the time being and 1 for number of weights
 //using averageWeight for kingWeight currently
-Neuron& evolveChild(Neuron& obj){
+Neuron& Neuron::evolveChild(){
     Neuron evolvedChild;
-    float evolvedWeight = obj.nodeWeightPrime(obj.getWeight(), obj.getRiskFactor());
-    float evolvedSigmaWeight = obj.sigmaWeightPrime(obj.getRiskFactor(), 1);
-    float evolvedKingWeight = obj.kingWeightPrime(obj.getAverageWeight());
+    float evolvedWeight = this->nodeWeightPrime(this->getWeight(), this->getRiskFactor());
+    float evolvedSigmaWeight = this->sigmaWeightPrime(this->getRiskFactor(), 1);
+    float evolvedKingWeight = this->kingWeightPrime(this->getAverageWeight());
 
+    evolvedChild.setWeight(evolvedWeight);
+    evolvedChild.setAverageWeight(evolvedSigmaWeight);
+    evolvedChild.setRiskFactor(evolvedKingWeight);
     return evolvedChild;
 }
 
 bool operator==(Neuron & lhs, Neuron & rhs)
 {
-	return ((lhs.getWeight() == rhs.getWeight()) &&
-		(lhs.getAverageWeight() == rhs.getAverageWeight()) &&
-		(lhs.getChildren() == rhs.getChildren()) &&
-		(lhs.getRiskFactor() == rhs.getRiskFactor()) &&
-		(lhs.getBoard() == rhs.getBoard()));
+    return ((lhs.getWeight() == rhs.getWeight()) &&
+            (lhs.getAverageWeight() == rhs.getAverageWeight()) &&
+            (lhs.getChildren() == rhs.getChildren()) &&
+            (lhs.getRiskFactor() == rhs.getRiskFactor()) &&
+            (lhs.getBoard() == rhs.getBoard()));
 }
 
 bool operator!=(Neuron & lhs, Neuron & rhs)
 {
-	return !(lhs == rhs);
+    return !(lhs == rhs);
 }
+
