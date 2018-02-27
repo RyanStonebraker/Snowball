689dec80c7ca81bd366d0e06c31ac301aef70f30
thatcherlane <thatcherlane@gmail.com>
2018-02-27 14:56:25 -0900

    adding a very basic test class for evolveChild()

---
+++ c++/test/testingNetworkEvlover.cpp
@@ -0,0 +1,11 @@
+//
+//  testing.cpp
+//  snowball
+//
+//  Created by Thatcher Lane on 2/27/18.
+//  Copyright © 2018 Thatcher Lane. All rights reserved.
+//
+
+#define CATCH_CONFIG_MAIN
+
+#include "catch2.h"
